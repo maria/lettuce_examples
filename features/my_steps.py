a1cb84a5fc4beb5b0e9c6d9899c74bc7c1516a57
marianitadn <maria.nita.dn@gmail.com>
2013-03-28 18:14:08 +0200

    added new scenario

--- features/my_steps.py
+++ features/my_steps.py
@@ -5,19 +5,38 @@ def i_have_number(step, number):
     world.number = int(number)
 
 @step('I compute his factorial')
-def compute_factorial(step):
-   world.factorial = factorial(world.number)
+def compute_his_factorial(step):
+    world.factorial = compute_factorial(world.number)
 
 @step('I get the number (\d+)')
-def get_the_number(step, factorial):
-    assert (world.factorial == factorial,
-        "They are not equal")
+def get_the_number(step, given_factorial):
+    assert world.factorial == int(given_factorial), \
+        "Got %s" % world.factorial
 
-def factorial(n):
+##################################################
+
+@step('I have the numbers (\d+), (\d+)')
+def i_have_numbers(step, a, b):
+    world.a = int(a)
+    world.b = int(b)
+
+@step('I compute addition')
+def compute_addition(step):
+    world.addition = add_numbers(world.a, world.b)
+
+@step('I get the sum (\d+)')
+def get_the_number(step, sum):
+    assert world.addition == int(sum), \
+        "Got %s" % world.addition
+
+def add_numbers(a, b):
+    return a + b
+
+def compute_factorial(n):
     if n == 1 or n == 0:
         return 1
     elif n > 1:
-        fact = factorial(n-1) + factorial(n-2)
+        fact = compute_factorial(n-1) + compute_factorial(n-2)
         return fact
     else:
         return 0
--- features/my_test.feature
+++ features/my_test.feature
@@ -3,7 +3,7 @@ Feature: Compute factorial
     As a developer
     I'll implement factorial
 
-    Scenario Outline: Factorials for numbers 0-4
+    Scenario Outline: Factorials for numbers 0-2
         Given I have the number <number>
         When I compute his factorial
         Then I get the number <factorial>
@@ -14,3 +14,11 @@ Feature: Compute factorial
             |1       | 1         |
             |2       | 2         |
 
+    Scenario Outline: Add the number 3 with 4
+        Given I have the numbers <number>, <factorial>
+        When I compute addition
+        Then I get the sum <sum>
+
+    Examples:
+            | number | factorial | sum  |
+            |3       | 4         | 7    |
